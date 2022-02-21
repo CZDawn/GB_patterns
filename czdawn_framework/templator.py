f5b375e662530bca5f3e0ce51393a476271c0b43
mininscode <mininscode@gmail.com>
2022-02-21 14:09:40 +0300

    Done home task for lesson 3

--- czdawn_framework/templator.py
+++ czdawn_framework/templator.py
@@ -3,8 +3,7 @@ This module implement logic of templator jinja2 work.
 '''
 
 from os.path import join
-from jinja2 import Environment, FileSystemLoader
-# from jinja2 import Template
+from jinja2 import FileSystemLoader, Environment
 
 
 def render(template_name, folder='templates', static_url='/static/', **kwargs):
@@ -13,7 +12,3 @@ def render(template_name, folder='templates', static_url='/static/', **kwargs):
     env.globals['static'] = static_url
     template = env.get_template(template_name)
     return template.render(**kwargs)
-    # path_to_file = join(folder, template_name)
-    # with open(path_to_file, encoding='utf-8') as file:
-    #     template = Template(file.read())
-    # return template.render(**kwargs)
